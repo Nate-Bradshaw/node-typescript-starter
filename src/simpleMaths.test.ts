b221ef6a6bfa32049c58f1c7252a2467623b4f5f
Nate Bradshaw <nate.bradshaw5959@gmail.com>
2021-11-18 09:38:10 +0000

    added rectangle and circle unit tests

--- src/simpleMaths.test.ts
+++ src/simpleMaths.test.ts
@@ -1,4 +1,4 @@
-import { calcAreaSquare, calcAreaTriangle } from './simpleMaths'
+import { calcAreaSquare, calcAreaTriangle, calcAreaRectangle, calcAreaCircle } from './simpleMaths'
 
 test('Area Square', () => {
     expect(calcAreaSquare(4)).toBe(16);
@@ -6,4 +6,12 @@ test('Area Square', () => {
 
 test('Area Triangle', () => {
     expect(calcAreaTriangle(4, 5)).toBe(10);
+})
+
+test("Area Rectangle", () => {
+    expect(calcAreaRectangle(10, 5)).toBe(50);
+})
+
+test("Rounded Area Circle", () =>{
+    expect(calcAreaCircle(5)).toBe(79);
 })
--- src/simpleMaths.ts
+++ src/simpleMaths.ts
@@ -6,3 +6,10 @@ export function calcAreaTriangle(width: number, height: number): number {
     return width * height / 2;
 }
 
+export function calcAreaRectangle(width: number, height: number): number { //very hard and clever algorithm
+    return width * height; 
+}
+
+export function calcAreaCircle(radius: number): number {
+    return Math.round(Math.PI * Math.pow(radius, 2));
+}
